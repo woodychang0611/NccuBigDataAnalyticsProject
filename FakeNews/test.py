fdac61f346a7d00f17c7f8eca4d19bdb260c4193
張天慈 <woodychang0611@gmail.com>
2019-10-22 09:57:08 +0800

    update for one hot

--- FakeNews/test.py
+++ FakeNews/test.py
@@ -1,24 +1,18 @@
-from numpy import loadtxt
 import pandas as pd
-from enum import Enum
-
-class FakeNewsLabel(Enum):
-  non_rumor = 0
-  false = 1
-  true = 2
-  unverified = 3
 
 def process_text(text):
   text_length = len(text)
   return [text_length]
 
-label = pd.read_csv("label.txt",sep=':',index_col=1,header=None,names=['label','index'])
-tweets = pd.read_csv("source_tweets.txt",sep='\t',index_col=0,header=None,names=['index','text'])
-tweets = pd.concat([tweets,label], axis=1)  
-tweets['text_length'] = tweets.text.apply(lambda s: process_text(s))
-print (tweets)
-for row in tweets.iterrows():
-  index = row[0]
-  text = row[1].text
+label_dataframe = pd.read_csv("label.txt",sep=':',index_col=1,header=None,names=['label','index'])
+one_hot_label_dataframe = pd.get_dummies(label_dataframe,prefix=['label'])
+label_dataframe = pd.concat([label_dataframe,one_hot_label_dataframe], axis=1) 
+tweets_dataframe = pd.read_csv("source_tweets.txt",sep='\t',index_col=0,header=None,names=['index','text'])
+tweets_dataframe['text_length'] = tweets_dataframe.text.apply(lambda s: process_text(s))
+tweets_dataframe = pd.concat([tweets_dataframe,label_dataframe], axis=1)  
+
+
+print (tweets_dataframe)
+print (tweets_dataframe.columns)
 
 
